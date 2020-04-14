8650b4d3d4483b512b85cefa6183760c1f39e706
liyakun <1315385515@qq.com>
2020-04-14 11:40:13 +0800

    fix bug

--- lib/sub_nav/sub_nav_widget.dart
+++ lib/sub_nav/sub_nav_widget.dart
@@ -1,9 +1,59 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
-import 'dart:async';
 import 'package:ctrip_gird_demo/sub_nav/sub_nav_sprites_image.dart';
 
-class SubNavWidget extends StatelessWidget {
+class SubNavWidget extends StatefulWidget {
+  @override
+  SubNavWidgetWidgetState createState() => SubNavWidgetWidgetState();
+}
+
+class SubNavWidgetWidgetState extends State<SubNavWidget> {
+  ui.Image _img;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSubNavImageByProvider(
+      AssetImage('images/un_ico_subnav.png'),
+    );
+  }
+
+  @override
+  void dispose() {
+    _img.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_img != null) {
+      return Column(
+        children: <Widget>[
+          _rowItemContent(0, _img),
+          _rowItemContent(1, _img),
+        ],
+      );
+    } else {
+      return Container(
+        height: 110,
+      );
+//      return FutureBuilder<ui.Image>(
+//        future: _loadSubNavImageByProvider(
+//          AssetImage('images/un_ico_subnav.png'),
+//        ),
+//        builder: (context, snapshot) {
+//          return Column(
+//            children: <Widget>[
+//              _rowItemContent(0, snapshot.data),
+//              _rowItemContent(1, snapshot.data),
+//            ],
+//          );
+//        },
+//      );
+    }
+  }
+
   final List<String> _names = [
     '自由行',
     'Wifi电话本',
@@ -19,23 +69,27 @@ class SubNavWidget extends StatelessWidget {
 
   final int _rowItemsNum = 5;
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<ui.Image>(
-      future: _loadSubNavImageByProvider(
-        AssetImage('images/un_ico_subnav.png'),
-      ),
-      builder: (context, snapshot) {
-        return Column(
-          children: <Widget>[
-            _rowItemContent(0, snapshot.data),
-            _rowItemContent(1, snapshot.data),
-          ],
-        );
-      },
-    );
+  _loadSubNavImageByProvider(
+    ImageProvider provider, {
+    ImageConfiguration config = ImageConfiguration.empty,
+  }) async {
+    debugPrint('load local image!');
+    Completer<ui.Image> completer = Completer<ui.Image>(); //完成的回调
+    ImageStreamListener listener;
+    ImageStream stream = provider.resolve(config); //获取图片流
+    listener = ImageStreamListener((ImageInfo frame, bool sync) {
+      //监听
+      final ui.Image image = frame.image;
+      setState(() {
+        _img = frame.image;
+      });
+      completer.complete(image); //完成
+      stream.removeListener(listener); //移除监听
+    });
+    stream.addListener(listener); //添加监听
   }
 
+  /*
   Future<ui.Image> _loadSubNavImageByProvider(
     ImageProvider provider, {
     ImageConfiguration config = ImageConfiguration.empty,
@@ -44,88 +98,48 @@ class SubNavWidget extends StatelessWidget {
     Completer<ui.Image> completer = Completer<ui.Image>(); //完成的回调
     ImageStreamListener listener;
     ImageStream stream = provider.resolve(config); //获取图片流
-    listener = ImageStreamListener(
-      (ImageInfo frame, bool sync) {
-        //监听
-        final ui.Image image = frame.image;
-        completer.complete(image); //完成
-        stream.removeListener(listener); //移除监听
-      },
-      // node: ignore `onError` and `onChunk`.
-    );
+    listener = ImageStreamListener((ImageInfo frame, bool sync) {
+      //监听
+      final ui.Image image = frame.image;
+      setState(() {
+        _img = frame.image;
+      });
+      completer.complete(image); //完成
+      stream.removeListener(listener); //移除监听
+    });
     stream.addListener(listener); //添加监听
     return completer.future;
   }
+   */
 
-  Widget _nomalItemExpanded(index, ui.Image img) {
+  Widget _itemWidget(index, ui.Image img) {
     return Expanded(
-      child: _itemContainer(index, img),
-      flex: 1,
-    );
-  }
-
-  Widget _itemExpandedWithNew(index, ui.Image img) {
-    return Expanded(
-      flex: 1,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          _itemContainer(index, img),
-          _newTagText(),
-        ],
-      ),
-    );
-  }
-
-  Widget _newTagText() {
-    return Positioned(
-      top: 0,
       child: Container(
-        padding: EdgeInsets.fromLTRB(4, 0, 4, 0),
-        decoration: BoxDecoration(
-          gradient:
-              LinearGradient(colors: [Color(0xfff94242), Color(0xffffa25f)]),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Text(
-          'NEW',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 10,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _itemContainer(index, ui.Image img) {
-    return Container(
-      height: 55,
-      child: Column(
-        children: <Widget>[
-          SubNavSpritesImage(
-            showIndex: index,
-            img: img,
-          ),
-          Text(
-            _names[index],
-            style: TextStyle(
-              color: Color(0xff222222),
-              fontSize: 11,
+        height: 55,
+        child: Column(
+          children: <Widget>[
+            SubNavSpritesImage(
+              showIndex: index,
+              img: img,
             ),
-          ),
-        ],
+            Text(
+              _names[index],
+              style: TextStyle(
+                color: Color(0xff222222),
+                fontSize: 11,
+              ),
+            ),
+          ],
+        ),
       ),
+      flex: 1,
     );
   }
 
   List<Widget> _rowContentList(int rowIdx, ui.Image img) {
     return List<int>.generate(_rowItemsNum, (i) => i + rowIdx * _rowItemsNum)
         .map((idx) {
-      if (idx == 8) {
-        return _itemExpandedWithNew(idx, img);
-      }
-      return _nomalItemExpanded(idx, img);
+      return _itemWidget(idx, img);
     }).toList();
   }
 
